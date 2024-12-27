8aeff52340be4faa4a12cd3c0f56bda3dcaf55ba
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-12-28 01:34:47 +0300

    onboarding screen added

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -14,7 +14,12 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let viewController = TabBarController()
+        let viewController = OnboardingViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        // TODO: check first launch or not 
+//        let viewController = TabBarController()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
