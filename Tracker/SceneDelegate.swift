85937bf05367fcae5a1bb60eccf23f2c5c45ff68
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-12-28 10:33:53 +0300

    first launch logic added

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -11,17 +11,42 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private enum Keys: String {
+        case isFirstLaunch
+    }
+    
+    private var isFirstLaunch: Bool {
+        get {
+            print("Getting return \(!UserDefaults.standard.bool(forKey: Keys.isFirstLaunch.rawValue))")
+            return !UserDefaults.standard.bool(forKey: Keys.isFirstLaunch.rawValue)
+        }
+        set {
+            print("Setting isFirstLaunch to: \(!newValue)")
+            UserDefaults.standard.set(!newValue, forKey: Keys.isFirstLaunch.rawValue)
+        }
+    }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let viewController = OnboardingViewController(
+        let tabBarController = TabBarController()
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
+        if isFirstLaunch {
+            isFirstLaunch = false
+            showOnboarding(tabBarController: tabBarController)
+        }
+    }
+    
+    private func showOnboarding(tabBarController: TabBarController) {
+        let onboardingViewController = OnboardingViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal
         )
-        // TODO: check first launch or not 
-//        let viewController = TabBarController()
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+        
+        onboardingViewController.modalPresentationStyle = .fullScreen
+        tabBarController.present(onboardingViewController, animated: true)
     }
 }
 
